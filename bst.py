59147c17c612ae2d91a1351e00fd6d3891ed7dd2
Chernyshov <xpom55@mail.ru>
2018-05-19 15:26:13 +0300

    something wrong with remove and travers in bst3 tree ... cant find it =(

--- bst.py
+++ bst.py
@@ -16,7 +16,7 @@ class BinarySearchTree(object):
       self.root = Node(data)
     else:
       self.insertNode(data, self.root);
-
+  ###
   # O(LogN) !!! if we have balanced tree else it can be O(N)
   # --> AVL RBT are needed for balancing !!!
   def insertNode(self, data, node):
@@ -32,10 +32,55 @@ class BinarySearchTree(object):
         node.rightChild = Node(data)
 
 
+  def remove(self, data):
+    if self.root:
+      self.root = self.removeNode(data, self.root)
+  ###
+  def removeNode(self, data, node):
+    if not node: # if node None ??? разве обязательно нужно ???
+      return node # нужно для установки у родителя None
+
+    if data < node.data: # в левом поддереве ищем
+      node.leftChild = self.removeNode(data, node.leftChild)
+    elif data > node.data: # в правом поддереве ищем
+      node.rightChild = self.removeNode(data, node.rightChild)
+    else: # нашли node
+
+      if not node.leftChild and not node.rightChild: # 1 случай - лист
+        print('[1] Remove a leaf node ... %s ' % data)
+        del node
+        return None
+
+      if not node.leftChild or not node.rightChild: # 2 случай - один ребенок
+        print('[2] Remove parent with one child ... %s ' % data)
+
+        tempNode = node.rightChild
+        if node.leftChild:
+          tempNode = node.leftChild
+
+        del node # удаляем найденный node
+        return tempNode # возвращаем вместо удаленного - сохраненный
+
+      # 3 случай - два ребенка
+      #  I  - в правом ищем MIN node (либо в левом MAX), меняем его
+      #  II - вызываем удаление с найденным MIN node (либо MAX)
+      print('[3] Remove parent with two childs ... %s ' % data)
+      minNode = self.getMinNode(node.rightChild) # нашли
+      print('minNode %s ' % minNode.data)
+      node.data = minNode.data # меняю значения на MIN в правом
+      node.rightChild = self.removeNode(minNode.data, node.rightChild) # удаляю в правом MIN значение
+  ###
+  def getMinNode(self, node): # находим MIN node в дереве
+    if node.leftChild:
+      return self.getMinNode(node.leftChild)
+
+    return node
+
+
   def getMinValue(self):
     if self.root: # if root not None
       return self.getMin(self.root)
-
+  ###
   def getMin(self, node):
     if node.leftChild: # have left Child
       return self.getMin(node.leftChild)
@@ -46,7 +91,7 @@ class BinarySearchTree(object):
   def getMaxValue(self):
     if self.root:
       return self.getMax(self.root)
-
+  ###
   def getMax(self, node):
     if node.rightChild:
       return self.getMax(node.rightChild)
@@ -57,7 +102,7 @@ class BinarySearchTree(object):
   def traverse(self):
     if self.root:
       self.traverseInOrder(self.root)
-
+  ###
   def traverseInOrder(self, node): # выводим по возрастанию
     # идем в самую левую и глубокую часть дереве и потом рекурсией
     # начинаем подниматься по дереву обратно
@@ -69,6 +114,8 @@ class BinarySearchTree(object):
     if node.rightChild:
       self.traverseInOrder(node.rightChild)
 
+
+# проверяем на нахождение MIN и MAX и вывод дерева
 bst = BinarySearchTree()
 bst.insert(10)
 bst.insert(5)
@@ -79,9 +126,33 @@ print(' MIN = %s ' % bst.getMinValue())
 print(' MAX = %f ' % bst.getMaxValue())
 bst.traverse()
 
+# проверяем на string значения и вывод
 bst2 = BinarySearchTree() # we can pass not only numbers =)
 bst2.insert('C')
 bst2.insert('B')
 bst2.insert('G')
 bst2.insert('Z')
 bst2.traverse()
+
+
+# проверяем на удаление элемента 1, 2 и 3 случаи
+bst3 = BinarySearchTree()
+bst3.insert(32)
+bst3.insert(10)
+bst3.insert(55)
+bst3.insert(79)
+bst3.insert(1)
+bst3.insert(19)
+bst3.insert(16)
+bst3.insert(23)
+
+bst3.traverse()
+print('  remove 10')
+bst3.remove(10)
+bst3.traverse()
+print('  remove 1')
+bst3.remove(1)
+bst3.traverse()
+print('  remove 55')
+bst3.remove(55)
+bst3.traverse()
